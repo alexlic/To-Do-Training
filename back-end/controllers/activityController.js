7314a7476adf5ba0765343c3f8f793dfc59aa712
bryanwuero <arroyo_bryan@hotmail.com>
2018-04-11 13:39:29 -0500

    Added the update activity controller

---
+++ back-end/controllers/activityController.js
@@ -0,0 +1,15 @@
+const ActivityModel = require('../models/activity') 
+
+//Update activity on list 
+const update = async function(req, res){
+    let id = await ActivityModel.findOneAndUpdate({_id: req.params.id}, req.body)
+    
+    try {
+        if(id){
+            res.status(200).send('Changed success!')
+        }
+    } catch (error) {
+        res.status(500).send('Error not activity found')
+        
+    }
+}
