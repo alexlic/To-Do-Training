bec0fe3a8119197d7a40b6dd77407784305f08f2
bryanwuero <arroyo_bryan@hotmail.com>
2018-04-11 16:16:18 -0500

    Router.js forgot to add the controller of update

--- back-end/router.js
+++ back-end/router.js
@@ -1,2 +1,6 @@
 const express = require('express')
 const Router = express.Router()
+
+const activityController = require('./controllers/activityController')
+
+activityController.update('/activity/:id', activityController.update)
