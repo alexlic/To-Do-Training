352b559a384edd89db0bb0fde0663016d2608089
alexlic <ruben.licea@unosquare.com>
2018-04-11 12:39:00 -0500

    adding route to get method

--- back-end/router.js
+++ back-end/router.js
@@ -1,2 +1,8 @@
 const express = require('express')
 const Router = express.Router()
+
+const activitiesController = require('./controllers/activitiesController')
+
+Router.get('/activities', activitiesController.get)
+
+module.exports = Router
