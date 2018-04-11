454c959e8ad8d54acbcba2250e41cf35cedb4e94
alexlic <ruben.licea@unosquare.com>
2018-04-11 12:39:31 -0500

    configured get method

---
+++ back-end/controllers/activitiesController.js
@@ -0,0 +1,17 @@
+const activityModel = require('../models/activity')
+
+const get = function ( req, res ) {
+    ActivityModel.find({}).exec( (err, result) => {
+        if(err){
+          console.log(err)
+          res.estatus(500).send({message: 'error in data data base'})
+        }
+        else{
+            result.length > 0 ? res.send(result) : res.send({message: 'there is not activities saved'})
+        }
+    })
+}
+
+module.exports = {
+    get
+}
