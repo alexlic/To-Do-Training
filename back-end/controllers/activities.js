e210816c463d97beef00f82f6166bdc50d0dfe99
alexlic <ruben.licea@unosquare.com>
2018-04-13 12:12:25 -0500

    chenged conexion string

--- back-end/controllers/activities.js
+++ back-end/controllers/activities.js
@@ -1,5 +1,6 @@
 const ActivityModel = require('../models/activity')
 
+//------------Get activities---------
 const getActivities = function ( req, res ) {
     ActivityModel.find({}).exec( (err, result) => {
         if(err){
@@ -9,7 +10,22 @@ const getActivities = function ( req, res ) {
         res.send(result)
     })
 }
+//------------Add activities-----------
+
+//------------Update activities---------
+
+//------------Delete activities---------
+const deleteOne = function (req, res) {
+    ActivityModel.findOneAndRemove({_id: req.params.id}, function(err, acttivity){
+        if(err){
+            return res.status(500).send("Something went wrong tryng to delete the activity")
+        }
+        res.send({message: 'Activity deleted'})
+    })
+}
+
 
 module.exports = {
-    getActivities
+    getActivities,
+    deleteOne
 }
